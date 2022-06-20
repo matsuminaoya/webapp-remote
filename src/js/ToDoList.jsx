7f96f1ac091e8841dc1ffec85955bb4c086d189e
paintabridge <103555295+paintabridge@users.noreply.github.com>
2022-06-21 03:28:56 +0900

    #10-hlaf

--- src/js/ToDoList.jsx
+++ src/js/ToDoList.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useContext } from 'react';
 import axios from 'axios';
 export const ToDoList = (props) => {
   const [items, setItems] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
   const [itemInput, setItemInput] = useState('');
+  const inputDeadlineRef = useRef();
+
   const getItems = async () => {
     try {
       setErrorMessage('');
@@ -16,11 +18,11 @@ export const ToDoList = (props) => {
       setErrorMessage(error.message);
     }
   };
-
   const handleItemInput = (event) => {
     setItemInput(event.target.value);
   };
   const addItem = async () => {
+    const deadline = inputDeadlineRef.current.value;
     try {
       setErrorMessage('');
       await axios({
@@ -28,13 +30,65 @@ export const ToDoList = (props) => {
         url: props.url,
         data: {
           task: itemInput,
-          completed: false
-        },
+          completed: false,
+          ...(deadline ? { deadline: deadline } : {})
+        }
       });
+      // 入力エリアをクリアする．
       setItemInput('');
+      inputDeadlineRef.current.value = '';
+      // ToDoリストを更新する
       getItems();
     } catch (error) {
       setErrorMessage(error.message);
+    }
+  };
+  const setItemCompleted = async (id, event) => {
+    const item = items.find((item) => item._id === id);
+    if (item) {
+      /* オブジェクトリテラルのスプレッド構文を使用 */
+      const copiedItem = { ...item, completed: event.target.checked };
+      try {
+        setErrorMessage('');
+        await axios({
+          method: 'put',
+          url: props.url + '/' + id,
+          data: copiedItem,
+        });
+        getItems();
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+    }
+  }
+  const deleteItem = async (id) => {
+    try {
+      setErrorMessage('');
+      await axios({
+        method: 'delete',
+        url: props.url + '/' + id,
+      });
+      // TODOリストを更新する．ここでは簡単にするためすべてのアイテムをリロードする．
+      getItems();
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  }
+  const setItemDeadline = async (id, event) => {
+    const item = items.find((item) => item._id === id);
+    if (item) {
+      const copiedItem = { ...item, deadline: event.target.value };
+      try {
+        setErrorMessage('');
+        await axios({
+          method: 'put',
+          url: props.url + '/' + id,
+          data: copiedItem
+        });
+        getItems();
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
     }
   };
 
@@ -52,11 +106,16 @@ export const ToDoList = (props) => {
           {items.map((row) => (
             <div className="todolist-task" key={row['_id']}>
               <span className="todolist-completed">
-                <input type="checkbox" checked={row['completed']} />
+                <input type="checkbox" checked={row['completed']}
+                  onChange={setItemCompleted.bind(null, row['_id'])} />
               </span>
               <span className={"todolist-description" +
                 (row['completed'] ? " todolist-done" : "")}>
                 {row['task']}</span>
+              <input type="date" value={row['deadline']}
+                onChange={setItemDeadline.bind(null, row['_id'])} />
+              <button type="button" onClick={deleteItem.bind(null, row['_id'])}>
+                削除</button>
             </div>
           ))}
         </div>}
@@ -64,6 +123,7 @@ export const ToDoList = (props) => {
       <div className="todolist-task-input">
         <input type="text" onChange={handleItemInput}
           placeholder="タスク" value={itemInput} />
+        <input type="date" ref={inputDeadlineRef} />
         <button type="button" onClick={addItem} disabled={itemInput.length === 0}>
           追加</button>
       </div>
